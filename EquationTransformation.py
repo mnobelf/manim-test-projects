75b0ceb19cdeddf576aa3af1e89691942ae5e1c6
mnobelf <mn.fauzan07@gmail.com>
2024-04-03 08:44:39 +0700

    adjusting timing

--- EquationTransformation.py
+++ EquationTransformation.py
@@ -13,11 +13,6 @@ class EquationTransformation(Scene):
         equation1.scale(equations[1][1])
         self.play(Write(equation1))
 
-        pmv = MathTex("P = mv").set_color(BLUE)
-        pmv.next_to(equation1, DOWN)
-        self.play(Write(pmv))
-        self.play(FadeOut(pmv))
-
         # Transform the equation into F = ma
         self.animate_equations(equation1, equations)
 
@@ -25,20 +20,25 @@ class EquationTransformation(Scene):
         for i, (text, scale) in enumerate(zip(eq_data[0][1:], eq_data[1][1:])):
             new_equation = MathTex(text)
             new_equation.scale(scale)
-            self.play(TransformMatchingShapes(eq, new_equation))
-            eq = new_equation
 
             # Add "m constant" if the current equation matches
             if text == eq_data[0][1]:
+                note = MathTex("P = mv").set_color(BLUE)
+                note.next_to(eq, DOWN)
+                self.play(Write(note))
+                self.wait(1)
+                self.play(FadeOut(note),TransformMatchingShapes(eq, new_equation))
+            if text == eq_data[0][2]:
                 note = Text("m konstan", font_size=30, color=BLUE)
                 note.next_to(eq, DOWN, buff=SMALL_BUFF)
                 self.play(Write(note))
-                self.play(FadeOut(note))
-
-            elif text == eq_data[0][2]:
+                self.wait(1)
+                self.play(FadeOut(note),TransformMatchingShapes(eq, new_equation))
+            elif text == eq_data[0][3]:
                 note = MathTex("a = \\frac{dv}{dt}").set_color(BLUE)
                 note.next_to(eq, DOWN)
                 self.play(Write(note))
-                self.play(FadeOut(note))
+                self.wait(1)
+                self.play(FadeOut(note),TransformMatchingShapes(eq, new_equation))
 
-            self.wait(1)
+            eq = new_equation
