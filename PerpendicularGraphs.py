5bf43dbcd0d9252fd7089eb11295634f6b500710
mnobelf <mn.fauzan07@gmail.com>
2024-04-08 05:24:01 +0700

    add angles and vectors

--- PerpendicularGraphs.py
+++ PerpendicularGraphs.py
@@ -24,7 +24,37 @@ class PerpendicularGraphs(Scene):
         graph1.set_color(BLUE)
         graph2.set_color(RED)
 
+        # Calculate the intersection point
+        x_intersection = (5 + 3) / (2 + 0.5)
+        y_intersection = func1(x_intersection)
+        intersection_point = axes.c2p(x_intersection, y_intersection)
+
+        # Create the vectors
+        vector1 = Arrow(intersection_point, axes.c2p(x_intersection + 2, func1(x_intersection + 2)), buff=0)
+        vector2 = Arrow(intersection_point, axes.c2p(x_intersection + 4, func2(x_intersection + 4)), buff=0)
+
+        # Set the colors
+        vector1.set_color(YELLOW)
+        vector2.set_color(GREEN)
+
+
+        # Create the horizontal line
+        horizontal_line = DashedLine(start=axes.c2p(x_intersection - 10, y_intersection), end=axes.c2p(x_intersection + 10, y_intersection))
+
+        # Create the angle indicators
+        angle_alpha = Angle(horizontal_line, vector1, radius=1, other_angle=False)
+        angle_beta = Angle(horizontal_line, vector2, radius=1, other_angle=True)
+
+        # Add labels for the angles
+        label_alpha = MathTex("\\alpha").next_to(angle_alpha, RIGHT).shift(UP*0.1)
+        label_beta = MathTex("\\beta").next_to(angle_beta, RIGHT).shift(DOWN*0.1)
+
         self.play(Create(axes))
         self.play(Create(graph1))
         self.play(Create(graph2))
+        self.play(Create(vector1))
+        self.play(Create(vector2))
+        self.play(Create(horizontal_line))
+        self.play(Create(angle_alpha), Write(label_alpha))
+        self.play(Create(angle_beta), Write(label_beta))
         self.wait(3)
